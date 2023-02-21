e7615aeb2c177b8012b7de77b88b3baf0ddba5bc
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-21 20:04:37 +0000

    2nd ex

---
+++ exercises/2.js
@@ -0,0 +1,7 @@
+// Access an Array's Contents Using Bracket Notation
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+ myArray[1] = "not b anymore"
+// Only change code above this line
+console.log(myArray);
