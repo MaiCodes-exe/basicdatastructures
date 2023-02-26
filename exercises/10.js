7268c8fa61f8b894a0811bbe890718ad4f2f9b44
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-26 21:52:37 +0000

    10th need reviewing

---
+++ exercises/10.js
@@ -0,0 +1,10 @@
+// Check For The Presence of an Element With indexOf()
+
+
+function quickCheck(arr, elem) {
+  if (arr.indexOf(elem) >= 0) {
+    return true;
+  }
+  return false;
+}
+console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
