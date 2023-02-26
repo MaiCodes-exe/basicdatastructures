8d1aec289b2c5fb8b66960043779f06534960fcd
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-26 21:43:30 +0000

    9th ex

---
+++ exercises/9.js
@@ -0,0 +1,8 @@
+// Combine Arrays with the Spread Operator
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence= ["learning", ...fragment,"is", "fun"]; // Change this line
+  return sentence;
+}
+
+console.log(spreadOut());
