98d2e388a84d07e773a0a69c2e0b6479b25f2308
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-06 22:10:00 +0000

    13th ex

--- exercises/13.js
+++ exercises/13.js
@@ -1,1 +1,16 @@
 // Add Key-Value Pairs to JavaScript Objects
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// Only change code below this line
+
+foods["bananas"] = 13;
+foods["grapes"] = 35;
+foods["strawberries"] = 27;
+
+// Only change code above this line
+
+console.log(foods);
