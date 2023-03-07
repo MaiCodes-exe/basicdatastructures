97fb11ea32c425355909320d357e60b9501908c4
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-07 20:21:26 +0000

    15th ex

--- exercises/15.js
+++ exercises/15.js
@@ -1,1 +1,17 @@
 // Access Property Names with Bracket Notation
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+return foods[scannedItem]
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
