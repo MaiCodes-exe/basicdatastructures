3470d4e2a515ff072c0265f11bf066b005ca6022
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-07 21:19:46 +0000

    18th needs reviewing

--- exercises/18.js
+++ exercises/18.js
@@ -1,2 +1,27 @@
 // Iterate Through the Keys of an Object with a for...in Statement
 
+const users = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+
+function countOnline(usersObj) {
+  // Only change code below this line
+  let result = 0;
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      result++;
+    }
+  }
+  return result;
+  // Only change code above this line
+}
+
+console.log(countOnline(users));
